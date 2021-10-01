28268ba9fd3c806f86f5724fcb0bd8be1a673633
alxk <alex@kaskaso.li>
2021-10-01 08:49:12 +0100

    update parameter names and description

--- cmd/main.go
+++ cmd/main.go
@@ -22,8 +22,8 @@ var (
 
 	githubCmd        = flag.NewFlagSet("github", flag.ExitOnError)
 	githubToken      = githubCmd.String("token", "", "The GitHub access token.")
-	githubApiURI     = githubCmd.String("github-api-uri", "https://api.github.com", "The target GitHub API URI, defaults to https://api.github.com; Change if targeting Github Enterprise.")
-	githubGraphQlURI = githubCmd.String("github-graphql-uri", "https://api.github.com/graphql", "The target GitHub GraphQL URI, defaults to https://api.github.com/graphql; Change if targeting Github Enterprise.")
+	githubRESTURL    = githubCmd.String("github-rest-url", "https://api.github.com", "The target GitHub API URL, defaults to https://api.github.com.")
+	githubGraphQLURL = githubCmd.String("github-graphql-url", "https://api.github.com/graphql", "The target GitHub GraphQL URL, defaults to https://api.github.com/graphql.")
 	githubIngestors  arrayFlags
 
 	circleCICmd    = flag.NewFlagSet("circleci", flag.ExitOnError)
